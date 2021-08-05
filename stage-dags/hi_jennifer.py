b7687808283f44fe26992e13013ce4809dd0349e
Felix Labayen <felixlabayen@yahoo.com>
2021-08-05 20:40:22 +0800

    FEATURE: Jennifer DAG

--- stage-dags/hi_jennifer.py
+++ stage-dags/hi_jennifer.py
@@ -2,16 +2,13 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-
 def print_hello():
- return 'Hello Felix'
+ return 'Hi Christelle'
 
-dag = DAG('hello_world', description='Hello world Batoy', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG('hi_Jennifer', description='Hi world Jennifer', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
 dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
 dummy_operator >> hello_operator
-
-dummy_operator >> multiplyby5_operator
